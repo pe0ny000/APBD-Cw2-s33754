59d04d0c18bdda3d0bc2267cb69339ddfb43b011
Julia Cembrowska <s33754@pjwstk.edu.pl>
2026-03-23 13:18:27 +0100

    Create class Projektor

---
+++ ConsoleApplication1/ConsoleApplication1/DaneSprzetowe/Laptop.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApplication1.DaneSprzetowe
+{
+    public class Laptop : Sprzet
+    {
+        public string Procesor{get;set;}
+        public double WielkoscEkranu{get;set;}
+
+        public Laptop(int sprzetId, string sprzetNazwa, string procesor, double wielkoscEkranu) : base(sprzetId, sprzetNazwa)
+        {
+            Procesor = procesor;
+            WielkoscEkranu = wielkoscEkranu;
+        }
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/DaneSprzetowe/Projektor.cs
@@ -0,0 +1,7 @@
+﻿namespace ConsoleApplication1.DaneSprzetowe
+{
+    public class Projektor
+    {
+        
+    }
+}
