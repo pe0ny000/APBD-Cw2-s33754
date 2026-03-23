b7e114c1dd35dc185c56847b45e6c33c63437a22
Julia Cembrowska <s33754@pjwstk.edu.pl>
2026-03-23 13:07:27 +0100

    Create class Sprzet

---
+++ ConsoleApplication1/ConsoleApplication1/DaneSprzetowe/Sprzet.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApplication1.DaneSprzetowe
+{
+    public class Sprzet
+    {
+        public int SprzetId { get; set; }
+        public string SprzetNazwa { get; set; }
+
+        public Sprzet(int sprzetId, string sprzetNazwa)
+        {
+            SprzetId = sprzetId;
+            SprzetNazwa = sprzetNazwa;
+        }
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,9 @@
+namespace ConsoleApplication1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
