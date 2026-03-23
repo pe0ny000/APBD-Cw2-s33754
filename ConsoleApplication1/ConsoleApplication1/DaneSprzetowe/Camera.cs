4c9516d543f378b24a67a78f115867879cb2896a
Julia Cembrowska <s33754@pjwstk.edu.pl>
2026-03-23 13:22:11 +0100

    Create class Sprzet

--- ConsoleApplication1/ConsoleApplication1/DaneSprzetowe/Camera.cs
+++ ConsoleApplication1/ConsoleApplication1/DaneSprzetowe/Camera.cs
@@ -1,7 +1,14 @@
 ﻿namespace ConsoleApplication1.DaneSprzetowe
 {
-    public class Camera
+    public class Camera : Sprzet
     {
-        
+        public double RozdzielczoscMPX { get; set; }
+        public string RodzajMatrycy { get; set; }
+
+        public Camera(int sprzetId, string sprzetNazwa, double rozdzielczoscMpx, string rodzajMatrycy) : base(sprzetId, sprzetNazwa)
+        {
+            RozdzielczoscMPX = rozdzielczoscMpx;
+            RodzajMatrycy = rodzajMatrycy;
+        }
     }
 }
--- ConsoleApplication1/ConsoleApplication1/DaneSprzetowe/Projektor.cs
+++ ConsoleApplication1/ConsoleApplication1/DaneSprzetowe/Projektor.cs
@@ -1,7 +1,14 @@
 ﻿namespace ConsoleApplication1.DaneSprzetowe
 {
-    public class Projektor
+    public class Projektor : Sprzet
     {
-        
+        public int Jasnosc{get;set;}
+        public bool PosiadaBloototh{get;set;}
+
+        public Projektor(int sprzetId, string sprzetNazwa, int jasnosc, bool posiadaBloototh) : base(sprzetId, sprzetNazwa)
+        {
+            Jasnosc = jasnosc;
+            PosiadaBloototh = posiadaBloototh;
+        }
     }
 }
