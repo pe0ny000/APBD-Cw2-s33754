31e54fa636ee6675cfec1d32ae04539504762fb7
Julia Cembrowska <s33754@pjwstk.edu.pl>
2026-03-23 14:08:47 +0100

    Create class Wypozyczenie

---
+++ ConsoleApplication1/ConsoleApplication1/DaneWypożyczenia/Wypozyczenie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApplication1.DaneWypożyczenia
+{
+    public class Wypozyczenie
+    {
+        public string DataWypozyczenia { get; set; }
+        public string DataPlanowanegoZwrotu { get; set; }
+        public string DataFaktycznegoZwrotu { get; set; }
+        public double KosztWypozyczenia { get; set; }
+
+        public Wypozyczenie(string dataWypozyczenia, string dataPlanowanegoZwrotu, string dataFaktycznegoZwrotu, double kosztWypozyczenia)
+        {
+            DataWypozyczenia = dataWypozyczenia;
+            DataPlanowanegoZwrotu = dataPlanowanegoZwrotu;
+            DataFaktycznegoZwrotu = dataFaktycznegoZwrotu;
+            KosztWypozyczenia = kosztWypozyczenia;
+        }
+    }
+}
